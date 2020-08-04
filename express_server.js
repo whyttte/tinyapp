c7d7a009360605e7b8ad3d04956bd70658ef15c7
whyttte <fogboruche@yahoo.com>
2020-08-04 00:28:41 +0000

    sending html

--- express_server.js
+++ express_server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
+ 
 app.get("/urls.json", (req, res) => {
   res.json("urlDatabase");
 });

5b7fd04b9f5a4ef3352ee295bfe954a51206428a
whyttte <fogboruche@yahoo.com>
2020-08-04 07:25:45 +0000

    end of generating random url

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+
 app.set('view engine', 'ejs');
 
 
@@ -10,6 +12,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let alphaNumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let randomGen = ''
+  for (let i = 0; i < 6; i++) {
+    randomGen += alphaNumeric.charAt(Math.floor(Math.random() * alphaNumeric.length));
+  }
+  return randomGen;
+}
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 // const id = {
 // // Route path: /users/:userId/books/:bookId
 // // Request URL: http://localhost:3000/users/34/books/8989
@@ -20,6 +34,17 @@ app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
